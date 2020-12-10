b13b85dbbab5410d368ff04d4817288872b06dce
JGWill <jgi@jgwill.com>
2020-12-10 15:18:13 -0500

    fix: code. working

--- train-dummy.py
+++ train-dummy.py
@@ -1,5 +1,10 @@
 from textgenrnn import textgenrnn
 
-textgen.train_from_file('akten-2016.txt', num_epochs=1)
+
+textgen = textgenrnn()
+
+textgen.train_from_file('datasets/akten-2016.txt', num_epochs=1)
 textgen.generate()
+
+
 
